7d29b3ab0325e0abd30b1eede0886e09a8a2d27c
KonTy <KonTy@github.com>
2024-07-04 22:46:04 -0700

    fixes

--- arch_post_install.sh
+++ arch_post_install.sh
@@ -76,23 +76,23 @@ declare -A prep_stage=(
     [xfconf]="xfconf-query library"
 )
 
-# declare -A hyprpaper_stage=(
-#     [hyprpaper]="Wall paper support"
-#     [gcc]="GNU Compiler Collection"
-#     [ninja]="Small build system with a focus on speed"
-#     [wayland-protocols]="Wayland protocols"
-#     [libjpeg-turbo]="JPEG image library"
-#     [libwebp]="WebP image library"
-#     [pango]="Text layout and rendering library"
-#     [cairo]="2D graphics library"
-#     [pkgconf]="Package compiler and linker metadata toolkit"
-#     [cmake]="Cross-platform make"
-#     [libglvnd]="The GL Vendor-Neutral Dispatch library"
-#     [wayland]="Wayland display server protocol"
-# )
-
 declare -A hyprpaper_stage=(
-    [swww]="change background"
+    [hyprpaper]="Wall paper support"
+    [gcc]="GNU Compiler Collection"
+    [ninja]="Small build system with a focus on speed"
+    [wayland-protocols]="Wayland protocols"
+    [libjpeg-turbo]="JPEG image library"
+    [libwebp]="WebP image library"
+    [pango]="Text layout and rendering library"
+    [cairo]="2D graphics library"
+    [pkgconf]="Package compiler and linker metadata toolkit"
+    [cmake]="Cross-platform make"
+    [libglvnd]="The GL Vendor-Neutral Dispatch library"
+    [wayland]="Wayland display server protocol"
+)
+
+declare -A swww_stage=(
+    [swww]="Change background"
 )
 
 declare -A audio_stage=(
@@ -267,7 +267,7 @@ echo -e "$CNT - Prep Stage - Installing needed components"
 install_software prep_stage
 
 echo -e "$CNT - Prep Stage - Installing hypprpaper components"
-install_software hyprpaper_stage
+install_software swww_stage
 
 echo -e "$CNT - Audio Stage - Installing audio components"
 install_software audio_stage
